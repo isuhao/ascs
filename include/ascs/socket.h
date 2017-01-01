f024d5e6cd20d16cd85b5baa26fffb62c7f155b3
youngwolf <mail2tao@163.com>
2017-01-01 13:12:45 +0800

    A trivial change.

--- include/ascs/socket.h
+++ include/ascs/socket.h
@@ -112,11 +112,7 @@ public:
 				lock.unlock();
 
 				if (!do_send_msg())
-				{
 					sending = false;
-					if (is_send_allowed() && !send_msg_buffer.empty())
-						send_msg(); //just make sure no pending msgs
-				}
 			}
 		}
 
@@ -276,11 +272,7 @@ protected:
 				lock.unlock();
 
 				if (!do_dispatch_msg())
-				{
 					dispatching = false;
-					if (!paused_dispatching && !recv_msg_buffer.empty())
-						dispatch_msg(); //just make sure no pending msgs
-				}
 			}
 		}
 
